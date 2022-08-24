c8da938dd38e0d0a1cb0e8bdd437f916e675a983
Pedrogabriel152 <105892538+Pedrogabriel152@users.noreply.github.com>
2022-08-24 19:28:03 -0300

    Java fixação

---
+++ Exercicios - Eng Soft/ExercicioConta.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class ExercicioConta{
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int numero;
+		int saldo;
+
+		System.out.println("Digite o numero da conta");
+			numero = sc.nextInt();
+
+		System.out.println("Digite o saldo da conta");
+			saldo = sc.nextInt();
+
+		System.out.println(numero);
+		System.out.println(saldo);
+
+
+	}
+}
